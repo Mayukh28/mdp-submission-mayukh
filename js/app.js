bf2a8e1f64d8facd8c709bd93e8f0d4fadccfba2
Mayukh28 <ov3rload28@gmail.com>
2022-02-01 09:24:17 +0530

    added backgrounds

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // This is the js file for functionality of whole website
+document.addEventListener("load", ()=>{})
 console.log("included");
 
 // consts
@@ -13,6 +14,14 @@ window.addEventListener("scroll", (e) => {
     header.classList.add('head-scrolled');
     header.classList.remove('head-not-scrolled');
     burger.style.color = '#000000'
+    // waves.style.display = "hidden"
+    const waves = document.getElementById('vanta-rings');
+    
+    waves.style.opacity = "80%";
+    // waves.style.height = `${100}vh`;
+    // waves.style.width = `${100}vw`;
+    // console.log(waves);
+    console.log(waves);
 
   } else if (window.scrollY < 221) {
     header.classList.remove("head-scrolled");
@@ -21,14 +30,31 @@ window.addEventListener("scroll", (e) => {
       scrldwn.innerHTML = `
       <a class="ca3-scroll-down-link ca3-scroll-down-arrow" data-ca3_iconfont="ETmodules" data-ca3_icon=""></a>
       `
+      const waves = document.getElementById('vanta-rings');
+    
+      waves.style.opacity = 0;
+      
   }
 });
+
+
+
+
+// hamburger menu
+
+
 var dropdown = document.querySelector("#dropdown");
 
 var hamburger = document.querySelector("#hamburger");
 c = 1;
 hamburger.addEventListener("click", () => {
-  c
+  // console.log("hamclicked")
+  c++;
+  if(c % 2 == 0) {
+    dropdown.style.height = `${60}px`
+} else {
+  dropdown.style.height = `${0}px`
+  }
 })
   // const number = prompt("Enter a number: ");
   
